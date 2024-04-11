62c23c790f5703ccec20de36835bd0b80b2325f6
Lakshman <marion.neumeier@thi.de>
2024-04-11 14:23:21 +0200

    Updates

---
+++ precache-manifest.1569e26223d7c2b98a9ef39ebd2f7124.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75abee7d5e9c92cea38f",
+    "url": "/VQSPEC/static/css/main.239377ab.chunk.css"
+  },
+  {
+    "revision": "75abee7d5e9c92cea38f",
+    "url": "/VQSPEC/static/js/main.75abee7d.chunk.js"
+  },
+  {
+    "revision": "d41fcd46c642c4af341e",
+    "url": "/VQSPEC/static/js/1.d41fcd46.chunk.js"
+  },
+  {
+    "revision": "ec87ecee2122891a16fe",
+    "url": "/VQSPEC/static/js/runtime~main.ec87ecee.js"
+  },
+  {
+    "revision": "eec0c14dc88f793621191b22afe62a23",
+    "url": "/VQSPEC/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pub/VQSPEC/precache-manifest.fa2d85ca0872328493ea48523deefd4a.js"
+  "/VQSPEC/precache-manifest.1569e26223d7c2b98a9ef39ebd2f7124.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/pub/VQSPEC/index.html", {
+workbox.routing.registerNavigationRoute("/VQSPEC/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
